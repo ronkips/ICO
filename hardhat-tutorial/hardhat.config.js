0788ffefed8a1f33b38cf9fd44d878246a808ac9
ronkips <kipshillash@gmail.com>
2022-09-27 20:55:35 +0300

    changed from Alchemy url to quicknode http url

--- hardhat-tutorial/hardhat.config.js
+++ hardhat-tutorial/hardhat.config.js
@@ -1,15 +1,14 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config({ path: ".env" });
-
-const ALCHEMY_API_KEY_URL = process.env.ALCHEMY_API_KEY_URL;
+const QUICKNODE_HTTP_URL = process.env.QUICKNODE_HTTP_URL;
 
 const GOERLI_PRIVATE_KEY = process.env.GOERLI_PRIVATE_KEY;
-
+/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.9",
   networks: {
     goerli: {
-      url: ALCHEMY_API_KEY_URL,
+      url: QUICKNODE_HTTP_URL,
       accounts: [GOERLI_PRIVATE_KEY]
     }
   }
